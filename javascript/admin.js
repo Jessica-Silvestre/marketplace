c04c52d84680d71a478a454f4113654c1de76c59
alexiactrindade <alexiadacostatrindade@gmail.com.br>
2026-05-20 15:32:05 -0300

    Página admin do marketplace

---
+++ javascript/admin.js
@@ -0,0 +1,28 @@
+'use strict'
+
+const ctx = document.getElementById('graficoCrescimento');
+
+new Chart(ctx, {
+  type: 'line',
+  data: {
+    labels: ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sáb'],
+    datasets: [
+      {
+        label: 'Agências ativas',
+        data: [200, 120, 380, 280, 180, 150, 300],
+        borderColor: '#8b5cf6'
+      },
+      {
+        label: 'Veículos publicados',
+        data: [120, 90, 290, 280, 90, 100, 300],
+        borderColor: '#22c55e'
+      }
+    ]
+  },
+  options: {
+    responsive: true,
+    plugins: {
+      legend: { display: false }
+    }
+  }
+}); 
